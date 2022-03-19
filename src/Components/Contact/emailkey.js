1d0c6829aa6fffac0e773b9d2fe024ccb85ed93d
riteshjk <96072567+riteshjk@users.noreply.github.com>
2022-03-19 15:50:16 +0530

    final

---
+++ src/Components/Contact/emailkey.js
@@ -0,0 +1,5 @@
+const USER_ID = `IaKJeUegqY8_JlPLy` //userID
+const TEMPLATE_ID = `template_x12t3zr`    //templateID
+const  SERVICE_ID = `service_lpa3nze`
+
+export {USER_ID, TEMPLATE_ID, SERVICE_ID}
